9412deb63f51901e5e1ae25364ccb2abc95f272b
wukunpeng <757246444@qq.com>
2017-04-28 15:33:37 +0800

    调整代码

--- src/main.js
+++ src/main.js
@@ -2,38 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
-import VueRouter from 'vue-router'
 import App from './App'
 import store from './store'
-import Home from './components/HelloFromVux'
-import { sync } from 'vuex-router-sync'
+import router from './router'
 
 require('es6-promise').polyfill()
-// 添加vue-router
-
-Vue.use(VueRouter)
-
-const routes = [{
-  path: '/',
-  component: Home
-}]
-
-const router = new VueRouter({
-  routes
-})
-// 路由切换时切换loading状态，主要在于优化拥护体验
-// 参考文档：https://vux.li/#/?id=%E9%A1%B5%E9%9D%A2%E5%88%87%E6%8D%A2%E6%98%BE%E7%A4%BAloading
-router.beforeEach(function (to, from, next) {
-  store.commit('updateLoadingStatus', {isLoading: true})
-  next()
-})
-
-router.afterEach(function (to) {
-  store.commit('updateLoadingStatus', {isLoading: false})
-})
-
-// 将路由的状态同步到store
-sync(store, router)
 
 // 添加Fastclick移除移动端点击延迟
 FastClick.attach(document.body)
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from '../store'
+import { sync } from 'vuex-router-sync'
+
+// components
 import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -13,3 +17,29 @@ export default new Router({
     }
   ]
 })
+
+// 路由切换时切换loading状态，主要在于优化拥护体验
+// 参考文档：https://vux.li/#/?id=%E9%A1%B5%E9%9D%A2%E5%88%87%E6%8D%A2%E6%98%BE%E7%A4%BAloading
+router.beforeEach(function(to, from, next) {
+  store.commit('updateLoadingStatus', {
+    isLoading: true
+  })
+  next()
+})
+
+router.afterEach(function(to) {
+  store.commit('updateLoadingStatus', {
+    isLoading: false
+  })
+  // 通过延迟设置loading状态可以查看效果
+  // setTimeout(function() {
+  //   store.commit('updateLoadingStatus', {
+  //     isLoading: false
+  //   })
+  // }, 1000)
+})
+
+// 将路由的状态同步到store
+sync(store, router)
+
+export default router
