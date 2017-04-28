908c33d7522ee2b9bfbdb13b32e02d1b54d7bb02
wukunpeng <757246444@qq.com>
2017-04-28 15:18:52 +0800

    增加注释

--- src/main.js
+++ src/main.js
@@ -21,7 +21,8 @@ const routes = [{
 const router = new VueRouter({
   routes
 })
-
+// 路由切换时切换loading状态，主要在于优化拥护体验
+// 参考文档：https://vux.li/#/?id=%E9%A1%B5%E9%9D%A2%E5%88%87%E6%8D%A2%E6%98%BE%E7%A4%BAloading
 router.beforeEach(function (to, from, next) {
   store.commit('updateLoadingStatus', {isLoading: true})
   next()
@@ -31,6 +32,7 @@ router.afterEach(function (to) {
   store.commit('updateLoadingStatus', {isLoading: false})
 })
 
+// 将路由的状态同步到store
 sync(store, router)
 
 // 添加Fastclick移除移动端点击延迟
